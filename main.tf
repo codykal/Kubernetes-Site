b79e029484b80acecf48e73e3b058db5317e6fe9
codykal <cody@TSDTECH-HVBNTD3.TSD.toppenish.wednet.edu>
2023-10-17 16:07:21 -0700

    Setting up Terraform Provider

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "ecsproject-tfstatebucket"
+    key            = "KSite/terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+    dynamodb_table = "my-lock-table"
+
+  }
+
+   required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
