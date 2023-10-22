6fbc4bf212a7356bec0a3beaed39688cbb2524c4
codykal <codyakal@gmail.com>
2023-10-22 13:03:08 -0700

    added terraform lock and initial EKS module

--- EKS.tf
+++ EKS.tf
@@ -7,12 +7,25 @@ module "eks" {
   cluster_version = "1.28"
   subnet_ids = [aws_subnet.Public1.id, aws_subnet.Public2.id]
 
+  cluster_endpoint_public_access = true
+
   vpc_id = aws_vpc.VPC_Main.id
   manage_aws_auth_configmap = true
 
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
     instance_types = ["t3.small"]
     iam_role_attach_cni_policy = true
+  }
+
+  eks_managed_node_groups = {
+    nginx = {
+      desired_size = 2
+      min_size = 1
+      max_size = 2
+
+      labels = {
+        server = "nginx"
+      }
+    }
   }
 }
