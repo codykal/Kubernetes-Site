4b00fdc63abea3b56537f13df37963a23f0bfcc3
Cody Kallenberger <codyakal@gmail.com>
2023-11-21 15:37:56 -0800

    Setting up IAM for Lambda

--- lambda.tf
+++ lambda.tf
@@ -1,4 +1,5 @@
 resource "aws_lambda_function" "push_to_efs" {
   function_name = "push_to_efs"
-  role = 
+  role = aws_iam_role.Lambda_Role.id
+  //TODO Make python script to push changes from S3 to EFS.
 }
